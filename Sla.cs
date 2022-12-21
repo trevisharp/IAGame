8372b77aa03411d891f52b6fc4036107e73580a3
ThiagoDSMarcelino <thiagodsmarcelino@gmail.com>
2022-12-21 16:42:16 -0300

    Teste

--- Sla.cs
+++ Sla.cs
@@ -1,65 +1,57 @@
-using System;
-using System.Drawing;
+// using System;
+// using System.Drawing;
 
-public class Sla : Player
-{
-    public Sla(PointF location) : 
-        base(location, Color.Purple, Color.Pink, "Sla") { }
-    
-    int frame = 0;
-    int countShoot = 0;
-    int stop = 0;
-    private PointF? target = null;
-    PointF ponto = new PointF();
-    PointF? flagDamage = null;
-    protected override void loop()
-    {
-        if (flagDamage != LastDamage)
-        {
-            float x = LastDamage.Value.X - this.Location.X;
-            float y = LastDamage.Value.Y - this.Location.Y;
-            StartTurbo();
-            StartMove(new PointF(-x, -y));
-        }
-        if (target == null)
-        {
-            if (frame % 5 == 0)
-                AccurateSonar();
-            if (EntitiesInAccurateSonar.Count != 0)
-                this.target = EntitiesInAccurateSonar[0];
-        }
-        else
-        {
-            stop = 0;
-            if (checkPosition())
-            {
-                target = null;
-                countShoot = 0;
-            }
-            else
-            {
-                if(countShoot <= 6)
-                {
-                    if (frame % 3 == 0)
-                    {
-                        Shoot(target.Value);
-                        countShoot++;
-                    }
-                }
-                else
-                {
-                    StartTurbo();
-                    StartMove(target.Value);
-                }
-            }
+// public class Sla : Player
+// {
+//     public Sla(PointF location) : 
+//         base(location, Color.Purple, Color.Pink, "Sla") { }
+//     int height = 1280;
+//     int whight = 800;
+//     int frame = 0;
+//     int countShoot = 0;
+//     private PointF? target = null;
+//     PointF ponto = new PointF();
+//     PointF? flagDamage = null;
+//     protected override void loop()
+//     {
+//         if (target == null)
+//         {
+            // if (frame % 5 == 0)
+            //     AccurateSonar();
+            // if (EntitiesInAccurateSonar.Count != 0)
+            //     this.target = EntitiesInAccurateSonar[0];
+//         }
+//         else
+//         {
+//             if (checkPosition())
+//             {
+//                 target = null;
+//                 countShoot = 0;
+//             }
+//             else
+//             {
+//                 if(countShoot <= 6)
+//                 {
+//                     if (frame % 3 == 0)
+//                     {
+//                         Shoot(target.Value);
+//                         countShoot++;
+//                     }
+//                 }
+//                 else
+//                 {
+//                     StartTurbo();
+//                     StartMove(target.Value);
+//                 }
+//             }
 
-        }
-        frame++;
-    }
+//         }
+//         frame++;
+//     }
 
-    private bool checkPosition(float range = 4f)
-        => this.Location.X >= this.target.Value.X - range
-        && this.Location.X <= this.target.Value.X + range
-        && this.Location.Y <= this.target.Value.Y + range
-        && this.Location.Y <= this.target.Value.Y + range;
-}
+    // private bool checkPosition(float range = 5f)
+    //     => this.Location.X >= this.target.Value.X - range
+    //     && this.Location.X <= this.target.Value.X + range
+    //     && this.Location.Y <= this.target.Value.Y - range
+    //     && this.Location.Y <= this.target.Value.Y + range;
+// }
