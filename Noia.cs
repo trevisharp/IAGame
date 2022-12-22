d54f446adb17dd06d18d1cd565b7b5c6f081a222
Bruno Oliveira Calisario <88956483+BrnCalisario@users.noreply.github.com>
2022-12-22 16:33:07 -0300

    chega

--- Noia.cs
+++ Noia.cs
@@ -27,18 +27,32 @@ public class Noia : Player
     PointF botRight = new PointF(screenWidth, screenHeigth);
 
     int bulletCounter = 0;
-    int maxBullets = 50;
+    int maxBullets = 100;
 
     int offset = 10;
     int restCount = 0;
 
+    int timer;
 
     PointF pontoSelecionado;
-
+    List<PointF> pontos = new List<PointF>();
+    int frame = 0;
 
     protected override void loop()
     {
-        List<PointF> pontos = new List<PointF> { topLeft, topRight, botRight, botLeft };
+        if(frame++ == 1)
+            pontos = new List<PointF> { topLeft, topRight, botRight, botLeft };
+
+        if(frame == 500)
+        {
+            InfraRedSensor(topLeft);
+        }
+
+        if(frame > 500 && Energy > 30 && EnemiesInInfraRed.Count > 0 && frame < 600)
+            Shoot(new PointF(0, 0));
+
+        if(frame > 600)
+            StopMove();
 
 
         if (Energy < 50)
@@ -48,7 +62,6 @@ public class Noia : Player
             turretMode = false;
             isloading = true;
         } else {
-            
         }
 
         if (isloading)
@@ -60,8 +73,11 @@ public class Noia : Player
             else return;
         }
 
-        if(LastDamage != ultimoDano)
+
+
+        if(LastDamage != ultimoDano && (DateTime.Now.Millisecond > timer + 5000))
         {   
+            timer = DateTime.Now.Millisecond;
             bulletCounter = maxBullets;
             StartTurbo();
             pontos.Reverse();       
@@ -73,8 +89,6 @@ public class Noia : Player
         if ((Location.X >= pontoSelecionado.X - 10 && Location.X <= pontoSelecionado.X + 10)
             && (Location.Y >= pontoSelecionado.Y - 10 && Location.Y <= pontoSelecionado.Y + 10))
         {
-            // if(shootCounter == 50)
-            //     shootCounter = 0;
             if(bulletCounter == maxBullets)
             {
                 inCorner = true;
@@ -131,8 +145,8 @@ public class Noia : Player
         if (bulletCounter < maxBullets && turretMode)
         {
             SizeF direction = new SizeF(
-                (float)Math.Cos((5f * i++) * (2 * Math.PI) / 360),
-                (float)Math.Sin((5f * i++) * (2 * Math.PI) / 360)
+                (float)Math.Cos((3f * i++) * (2 * Math.PI) / 360),
+                (float)Math.Sin((3f * i++) * (2 * Math.PI) / 360)
             );
 
             bulletCounter++;
