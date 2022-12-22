db3400c36bfb1c20da31933212cd6671debc9cd5
Bruno Oliveira Calisario <88956483+BrnCalisario@users.noreply.github.com>
2022-12-22 13:48:13 -0300

    Noia V3

--- Noia.cs
+++ Noia.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.Collections.Generic;
 
 public class Noia : Player
 {
@@ -8,80 +9,131 @@ public class Noia : Player
     { }
 
     int i = 0;
-    int count = 0;
-    PointF? enemy = null;
-    bool isloading = false;
-    int points = 0;
-    int frame = 0;
-    int searchindex = 0;
 
-    bool searchingFood = true;
+    const float screenWidth = 1280;
+    const float screenHeigth = 800;
+
+    bool isloading = false;
+    bool findWay = false;
     bool turretMode = true;
 
+    PointF topLeft = new PointF(0, 0);
+    PointF topRight = new PointF(screenWidth, 0);
+    PointF botLeft = new PointF(0, screenHeigth);
+    PointF botRight = new PointF(screenWidth, screenHeigth);
+
+    int offset = 10;
+
+    // List<PointF> pontos = new List<PointF> { new PointF(0, 0), new PointF(screenWidth, 0), new PointF(0, screenHeigth), new PointF(screenWidth, screenHeigth)};
+
+    PointF pontoSelecionado;
+
     protected override void loop()
-    {   
-        if(Energy > 50)
-            StartTurbo();
-        else
-        {
-            enemy = null;
-            StopTurbo();
-        }
-        
+    {
+
+        // StopMove();
+        StartTurbo();
+
         if (Energy < 10)
         {
             StopMove();
             isloading = true;
         }
+
         if (isloading)
-        {   
-            if(Location.X < 10 && Location.Y < 10)
-            {
-                turretMode = true;
-                StopMove();
-            }
-            else
-                StartMove(new PointF(0, 0));
-
-
-            if (Energy > 50)
+        {
+            if (Energy > 30)
                 isloading = false;
             else return;
         }
 
 
-        if(!turretMode)
-        {
-            if (EnemiesInInfraRed.Count == 0)
-            {
-                InfraRedSensor(1f * i++);
-            }
-            else
-            {
-                enemy = EnemiesInInfraRed[0];
-                ResetInfraRed();
-            }
-        }
-
-        if (enemy != null)
-        {
-            StartMove(new PointF(enemy.Value.X + 200, enemy.Value.Y + 200));
-        }
-
-        if(enemy == null)
+        if ((Location.X >= pontoSelecionado.X - 10 && Location.X <= pontoSelecionado.X + 10)
+            && (Location.Y >= pontoSelecionado.Y - 10 && Location.Y <= pontoSelecionado.Y + 10))
         {
             StopMove();
+            turretMode = true;
         }
 
-        if (Energy > 50)
+        if (!findWay)
         {
-            if (i++ % 1 == 0)
+            if (Location.X < screenWidth / 2 && Location.Y < screenHeigth / 2)
+            {
+                StartMove(topLeft);
+                pontoSelecionado = topLeft;
+            }
+
+            else if (Location.X >= screenWidth / 2 && Location.Y < screenHeigth / 2)
+            {
+                StartMove(topRight);
+                pontoSelecionado = topRight;
+            }
+
+            else if (Location.X >= screenWidth / 2 && Location.Y >= screenHeigth / 2)
+            {
+                StartMove(botRight);
+                pontoSelecionado = botRight;
+            }
+
+            else if (Location.X < screenWidth / 2 && Location.Y >= screenHeigth / 2)
+            {
+                StartMove(botLeft);
+                pontoSelecionado = botLeft;
+            }
+
+            findWay = true;
+        }
+
+
+        if (Energy > 60 && turretMode)
+        {
+            SizeF direction = new SizeF(
+                (float)Math.Cos((3f * i++) * (2 * Math.PI) / 360),
+                (float)Math.Sin((3f * i++) * (2 * Math.PI) / 360)
+            );
+
+            if(pontoSelecionado == botRight)
             {
 
-                SizeF direction = new SizeF(
-                    (float)Math.Cos((1f * i++) * (2 * Math.PI) / 360f),
-                    (float)Math.Sin((1f * i++) * (2 * Math.PI) / 360f)
-                );
+                if(direction.Height > 0)
+                {
+                    direction.Height *= -1;
+                } 
+
+                if(direction.Width > 0)
+                {
+                    direction.Width *= -1;
+                }
+
+                Shoot(Location + direction);
+            } 
+            else if(pontoSelecionado == topLeft)
+            {
+                if(direction.Height < 0)
+                    direction.Height *= -1;
+
+                if(direction.Width < 0)
+                    direction.Width *= -1;
+
+                Shoot(Location + direction);
+            }
+            else if(pontoSelecionado == topRight)
+            {
+                if(direction.Height < 0)
+                    direction.Height *= -1;
+            
+                if(direction.Width > 0)
+                    direction.Width *= -1;
+
+                Shoot(Location + direction);
+            }
+            else if(pontoSelecionado == botLeft)
+            {
+                if(direction.Height > 0)
+                    direction.Height *= -1;
+            
+                if(direction.Width < 0)
+                    direction.Width *= -1;
 
                 Shoot(Location + direction);
             }
