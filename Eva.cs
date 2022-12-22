a973afc6d1ffa8904dd404170f5c676d04f87e9e
LeonardoFalango <81644264+leonardofalango@users.noreply.github.com>
2022-12-22 14:46:19 -0300

    OPTMUS PRIME :)

--- Eva.cs
+++ Eva.cs
@@ -1,64 +1,152 @@
 using System;
 using System.Drawing;
-using System.Linq;
 
-public class Atirante : Player
+public class Eva : Player
 {
-    public Random rand { get; set; } = new Random();
-    public PointF dest { get; private set; }
-    public int frame { get; set; }
-    public bool isLoading { get; set; } = false;
-    public Atirante(PointF location) :
-        base(location, Color.Purple, Color.White, "Malvadão")
-    { this.dest = new PointF((int)rand.NextInt64(0, 1200),(int)rand.NextInt64(0, 800)); }
+    public Eva(PointF location) :
+        base(location, Color.GreenYellow, Color.SlateBlue, "Autobosts")
+    { 
+        int width = 1920;
+        int height = 1080;
 
+        isEsq = this.Location.X <= width / 2;
+        isTop = this.Location.Y <= height / 2;
+        PointF destFinal;
+        int corrX = 0;
+        int corrY = 0;
+        
+
+        if (isEsq && isTop)
+        {
+            destFinal = new PointF(0,0);
+            corrX = 100;
+            corrY = 100;
+            quadShoot = 0;
+        }
+        else if (isEsq && !isTop)
+        {
+            destFinal = new PointF(0,height);
+            corrX = 100;
+            corrY = 100;
+            quadShoot = 3;
+        }
+        else if (!isEsq && isTop)
+        {
+            destFinal = new PointF(width, 0);
+            corrX = -100;
+            corrY = 100;
+            quadShoot = 1;
+        }
+        else
+        {
+            destFinal = new PointF(width, height);
+            corrX = -100;
+            corrY = -100;
+            quadShoot = 2;
+        }
+
+        destStart = new PointF(destFinal.X + corrX, destFinal.Y + corrY);
+        angles[0] = new int[2] {0, 90};
+        angles[1] = new int[2] {91, 180};
+        angles[2] = new int[2] {181, 270}; 
+        angles[3] = new int[2] {271, 359};
+
+        Corr = new PointF((int)rand.NextInt64(50), (int)rand.NextInt64(50));
+    }
+    int mod = 0;
+    int tiro = 0;
+    int index = 0;
+    int frame = 0;
+    PointF? enemy = null;
+    public PointF Corr { get; set; }
+    public int quadShoot { get; private set; }
+    public bool isEsq { get; private set; }
+    public bool isTop { get; private set; }
+    public PointF destStart { get; private set; }
+    public int[][] angles { get; private set; } = new int[4][];
+    Random rand = new Random();
     protected override void loop()
     {
-        if (isLoading)
+        if (this.Energy <= 10)
+            mod = 2;
+        if (mod == 0)
         {
-            if (this.Energy > 30)
-                isLoading = false;
+            StartTurbo();
+            StartMove(destStart);
+            if (this.Location
+            .isInside (
+                    new PointF (
+                        destStart.X - 75, destStart.Y - 75
+                    ),
+                    new PointF (
+                        destStart.X + 70, destStart.Y + 70
+                    )
+                ))
+            {
+                mod = 1;
+                StopMove();
+            }
             return;
         }
 
-        PointF? enemy = null;
-        if (EnemiesInInfraRed.Count > 0)
-            enemy = EnemiesInInfraRed[0];
-        else
-            enemy = null;
-        
-        if (this.Location.X == dest.X && this.Location.Y == dest.Y)
-            dest = new PointF((int)rand.NextInt64(0, 1200),(int)rand.NextInt64(0, 800));
-        
-        if (this.Energy > 5)
+        if (mod == 1)
         {
-            if (enemy == null)
-                InfraRedSensor((int)rand.NextInt64(0, 306));
-            if (EnemiesInInfraRed.Count > 0)
+            if (angles[quadShoot][0] + index * 2 >= angles[quadShoot][1])
+                index = 0;
+            InfraRedSensor(angles[quadShoot][0] + index * 2);
+            index++;
+
+            if (EnemiesInInfraRed.Count >= 1 && enemy == null)
             {
-                Shoot(EnemiesInInfraRed[0]);
                 enemy = EnemiesInInfraRed[0];
             }
 
-            StartMove(dest);
-            PointF ponto = new PointF((int)rand.NextInt64(), (int)rand.NextInt64());
-            Shoot(ponto);
+            if (enemy != null && tiro < 10)
+            {
+                Shoot(enemy.Value);
+                tiro++;
+            }
+            if (tiro == 10)
+            {
+                enemy = null;
+                tiro = 0;
+            }
         }
-        else
-        {
-            StopMove();
-            isLoading = true;
-        }
+
         frame++;
-    }
+        if (frame % 40 == 0)
+        {
+            PointF x = new PointF(this.destStart.X + (int)rand.NextInt64(250 + (int)Corr.X), this.destStart.Y + (int)rand.NextInt64(250 + (int)Corr.Y));
+            StartMove(x);
+        }
 
-    private double hip(double X, double Y)
+        
+
+        if (mod == 2) {
+            StopTurbo();
+        }
+        
+
+    }
+}
+
+public static class Extensions {
+    public static bool isInside(this PointF myLocation, PointF start, PointF end)
     {
-        double difX = this.Location.X - X;
-        double difY = this.Location.Y - Y;
+        if (!(start.X < myLocation.X))
+            return false;
+        
+        if (!(myLocation.X <= end.X))
+            return false;
 
-        double catetosAoQuadrado = Math.Pow(difX, 2) + Math.Pow(difY, 2); 
 
-        return Math.Pow(catetosAoQuadrado, 0.5);
+        if (!(start.Y < myLocation.Y))
+            return false;
+        
+        if (!(myLocation.Y <= end.Y))
+            return false;
+        
+        return true;
     }
+
 }
